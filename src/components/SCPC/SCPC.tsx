ba8e1cc989b261b5ac87fc1daec76a87c1e6affb
Eric Jiang <ericjiang107@gmail.com>
2021-10-12 17:14:16 -0400

    updated project summary

--- src/components/SCPC/SCPC.tsx
+++ src/components/SCPC/SCPC.tsx
@@ -26,11 +26,12 @@ export const SCPC = () => {
                         the sites that I use to keep track of the market prices of cards doesn't quite satisfy what I wanted to do. Hence, I went ahead and created this.
                     </p>
                     <p className="text3">
-                        In order to accomplis the task, I accessed the TCG player API which is where market prices are often trusted and wher you can buy and sell cards.
-                        Knowing this, I focused mainly on the card game of the specific cards I wanted to keep track in order to keep things simple. By fetching and 
-                        parsing the data, I was able to showcase all the cards from each release set while adding a filter system which assisted in card tracking. 
-                        The final goal was to design and create a graph that showcases the changes in the prices which was done using Recharts and the data acquired
-                        from the api. Once completed, I was able to bookmark specific cards to my account and view them whenever I want through firestore. 
+                        In order to accomplish the task, I accessed the TCG player API which is where market prices are often trusted and where you can buy and 
+                        sell cards. Knowing this, I focused mainly on the card game of the specific cards I wanted to keep track in order to keep things simple. 
+                        By fetching and parsing the data by using GET and POST requests, I was able to showcase all the cards from each release set by adding a map 
+                        function while adding a filter system through JavaScript which assisted in card tracking. The final goal was to design and create a graph that 
+                        showcases the changes in the prices which was done using Recharts and the data acquired from the API. Once completed, I was able to bookmark 
+                        specific cards to my account and view them whenever I want through utilizing Google Cloud linking Firestore and Firebase to one another.
                     </p>
                 </div>
                 <div className="tech">
